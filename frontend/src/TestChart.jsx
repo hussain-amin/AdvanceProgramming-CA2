6ce9b6468bc6a6fe7ecb77b7e8c75a3fbd0a41b7
hussain-amin <hussainamin1090@gmail.com>
2025-12-11 19:26:48 +0000

    reorganised project structure frontend

--- frontend/src/TestChart.jsx
+++
@@ -1,9 +0,0 @@
-import { LineChart, Line } from "recharts";
-
-export default function TestChart() {
-  return (
-    <LineChart width={300} height={150} data={[{ x:1, y:2 }]}>
-      <Line dataKey="y" />
-    </LineChart>
-  );
-}
